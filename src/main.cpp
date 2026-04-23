4ae2acb24bed5cf371652e97dab4009c15e6c911
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2026-04-23 09:53:25 +0200

    add prism sampling to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,17 @@
+#include <chrono>
+#include <fstream>
 #include <iostream>
 #include <vector>
 
 #include "sampler/ISampler.h"
 #include "sampler/Sampler.h"
+#include "shape/Prism.h"
 
 int main() {
     std::cout << "Hello, Raytracer!\n";
+
+    std::ofstream prismSamples("prismSamples.csv");
+    prismSamples << "x,y,z\n";
 
     int resolution = 512;
     int nSamples = 64;
@@ -16,6 +22,8 @@ int main() {
     // Initialise the sampler cache
     auto cache = OqmcPmjBnSampler::createCache();
 
+    Prism prism(Point3f(0.f, 0.f, 0.f), Point3f(1.f, 0.f, 0.f), Point3f(0.5f, 1.f, 0.f), 2.f);
+
     // Loop over all pixels
     for (int y = 0; y < resolution; ++y) {
         for (int x = 0; x < resolution; ++x) {
@@ -23,14 +31,19 @@ int main() {
 
             // Loop over samples
             for (int index = 0; index < nSamples; ++index) {
+                auto start = std::chrono::high_resolution_clock::now();
                 // root domain (per pixel + sample)
-                OqmcPmjBnSampler pixelSampler(x, y, 0, index, cache);
+                OqmcPmjBnSampler baseSampler(x, y, 0, index, cache);
+                auto shapeSampler = baseSampler.split(ISampler::DomainKey::Shape);
+                auto lightSampler = baseSampler.split(ISampler::DomainKey::Light);
 
-                // create a subdomain for pixel sampling
-                auto sampleDomain = pixelSampler.split(ISampler::DomainKey::Light);
+                // sample points on the shape
+                const auto prismPoint = prism.sample(*shapeSampler);
+                // std::cout << prismPoint[0] << "," << prismPoint[1] << "," << prismPoint[2] << "\n";
+                // prismSamples << prismPoint[0] << "," << prismPoint[1] << "," << prismPoint[2] << "\n";
 
                 // Draw sample (2D)
-                auto sample = sampleDomain->next2D();
+                auto sample = lightSampler->next2D();
 
                 float xOffset = x + sample[0];
                 float yOffset = y + sample[1];
@@ -39,6 +52,9 @@ int main() {
                 if (xOffset * xOffset + yOffset * yOffset < resolution * resolution) {
                     pixelValue += 1.0f;
                 }
+                auto end = std::chrono::high_resolution_clock::now();
+                auto total_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+                std::cout << "time per sample: " << total_ns << " ns" << "\n";
             }
 
             // normalize
@@ -47,12 +63,6 @@ int main() {
     }
 
     std::cout << "Rendering complete.\n";
-
-    // dump a few values to verify it's working
-    std::cout << "Sample pixels:\n";
-    for (int i = 0; i < 10; ++i) {
-        std::cout << image[i] << "\n";
-    }
 
     return 0;
 }
