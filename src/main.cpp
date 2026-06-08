50320b95772b580a156f317f753727ef67a12cdd
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2026-06-08 20:58:03 +0200

    implement first integrator loop

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,44 @@
 #include "detector/PinHoleDetector.h"
 #include "light/SlitLight.h"
 #include "light/Spectrum.h"
+#include "physics/Snell.h"
 #include "sampler/ISampler.h"
 #include "sampler/Sampler.h"
 #include "shape/Prism.h"
 #include "types/Medium.h"
 
+constexpr float kEpsilon = 1e-5f;
+
+inline std::optional<Ray> refractThroughSurface(const IShape& shape, const Ray& incoming, float n1, float n2) {
+    // FIXME: explicit conversion Vector3 to Point3 needed
+    const auto rayOrigin = Point3f({incoming.origin.x, incoming.origin.y, incoming.origin.z});
+    const auto offsetOrigin = rayOrigin + Vector3f(incoming.direction.x * kEpsilon, incoming.direction.y * kEpsilon,
+                                                   incoming.direction.z * kEpsilon);
+    const auto hit = shape.intersect(offsetOrigin, incoming.direction);
+
+    if (!hit) return std::nullopt;
+
+    auto normal = hit.value().normal;
+
+    const auto refracted = SnellLaw(n1, n2, incoming.direction, normal);
+    if (!refracted) return std::nullopt;
+
+    // FIXME: explicit conversion Point3 to Vector3 needed
+    const auto point = Vector3f({hit->point.x, hit->point.y, hit->point.z});
+
+    return Ray{point, refracted.value()};
+}
+
 int main() {
     std::cout << "Hello, Raytracer!\n";
 
-    int resolution = 512;
-    int nSamples = 64;
+    std::ofstream intersections1("intersections1.csv");
+    intersections1 << "x,y,z\n";
+    std::ofstream intersections2("intersections2.csv");
+    intersections2 << "x,y,z\n";
+
+    int resolution = 64;
+    int nSamples = 12;
 
     // Image buffer (grayscale)
     std::vector<float> image(resolution * resolution, 0.0f);
@@ -24,12 +52,21 @@ int main() {
     auto cache = OqmcPmjBnSampler::createCache();
 
     SellmeierMedium prismMaterial = NBK7;
-    Prism prism(Point3f(0.f, 0.f, 0.f), Point3f(1.f, 0.f, 0.f), Point3f(0.5f, 1.f, 0.f), 2.f, prismMaterial);
-    SlitLight slitLight = SlitLight::from(Vector3f(-0.5f, -0.5f, -1.0f), Vector3f(0.5f, -0.5f, -1.0f),
-                                          Vector3f(-0.5f, 0.5f, -1.0f), DebugSpectrum);
-    PinHoleDetector detector =
-        PinHoleDetector::from(Vector3f(-0.5f, -0.5f, 1.0f), Vector3f(0.5f, -0.5f, 1.0f), Vector3f(-0.5f, 0.5f, 1.0f),
-                              Vector3f(0.0f, 0.0f, 0.0f), resolution, resolution);
+    Prism prism(Point3f(0.5f, -0.5f, 0.f), Point3f(1.5f, 1.f, 0.f), Point3f(2.5f, -0.5f, 0.f), 0.3f, prismMaterial);
+    SlitLight slitLight = SlitLight::from(Vector3f(0.0f, 0.0f, 0.0f), Vector3f(0.0f, 0.05f, 0.0f),
+                                          Vector3f(0.0f, 0.0f, 0.5f), DebugSpectrum);
+
+    const auto detectorOrigin = Vector3f(5.f, -0.5f, 0.15f);
+    const auto detectorNormal = Vector3f(-1.5f, 0.5f, 0.f).normalized();
+    const auto detectorBottomRight = Vector3f(4.5f, -1.5f, 0.15f);
+    const auto detectorTopLeft = detectorOrigin + Vector3f(0.f, 0.f, 0.15f);
+    const auto focalLength = 0.15f;
+
+    PinHoleDetector detector = PinHoleDetector::from(
+        detectorOrigin,       // origin
+        detectorBottomRight,  // bottomRight
+        detectorTopLeft,      // topLeft
+        detectorOrigin + detectorNormal * focalLength + detectorBottomRight * 0.5f, resolution, resolution);
 
     // Loop over all pixels
     for (int y = 0; y < resolution; ++y) {
@@ -43,34 +80,26 @@ int main() {
                 OqmcPmjBnSampler baseSampler(x, y, 0, index, cache);
                 auto detectorSampler = baseSampler.split(ISampler::DomainKey::Detector);
 
-                const auto ray = detector.sampleRay(x, y, *detectorSampler);
+                const auto detectorRay = detector.sampleRay(x, y, *detectorSampler);
 
-                //  shape intersection
-                const auto rayOrigin = Point3f(
-                    {ray.origin.x, ray.origin.y, ray.origin.z});  // FIXME: explicit conversion Vector3 to Point3
-                const auto intersection = prism.intersect(rayOrigin, ray.direction);
+                // intersection 1: entry into prism (air -> glass)
+                const auto refracted1 = refractThroughSurface(prism, detectorRay, 1.000277f,
+                                                              1.3f);  // TODO: correct refractive index calculation
+                if (!refracted1) continue;
 
-                if (!intersection.has_value()) {
-                    break;
-                }
-                const auto intersection_point = intersection.value().point;
-                const auto intersection_normal = intersection.value().normal;
+                // std::cout << "HIT";
 
-                // More complex E2E test than the circle
-                if (x == 0 && y == 0 && index == 0) {
-                    std::cout << "First detector ray origin: " << ray.origin.x << ", " << ray.origin.y << ", "
-                              << ray.origin.z << "\n";
+                // intersection 2: exit from prism (glass -> air)
+                const auto refracted2 = refractThroughSurface(prism, *refracted1, 1.3f,
+                                                              1.000277f);  // TODO: correct refractive index calculation
+                if (!refracted2) continue;
 
-                    std::cout << "First detector ray direction: " << ray.direction.x << ", " << ray.direction.y << ", "
-                              << ray.direction.z << "\n";
+                // TODO: check for intersection with lightsource
 
-                    std::cout << "First shape intersetion: " << intersection_point.x << ", " << intersection_point.y
-                              << ", " << intersection_point.z << "\n";
-                }
-
-                if (ray.direction.z > 0.0f) {
-                    pixelValue += 1.0f;
-                }
+                const auto& p1 = refracted1->origin;
+                intersections1 << p1.x << "," << p1.y << "," << p1.z << "\n";
+                const auto& p2 = refracted2->origin;
+                intersections2 << p2.x << "," << p2.y << "," << p2.z << "\n";
 
                 auto end = std::chrono::high_resolution_clock::now();
                 auto total_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
